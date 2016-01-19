f00aeb63f0d22c9ac9fd4fb25e94b2b7fa904d9f
Skmsoumya <soumyakishormishra@gmail.com>
2016-01-19 21:58:16 +0530

    added mobile support

--- dep/js/main.js
+++ dep/js/main.js
@@ -3,6 +3,8 @@
 $(document).ready(function() {
 	getPostData();
 	
+
+	
 });
 
 // Doing a AJAX call to get Posts data in JSON format from the server
@@ -12,7 +14,6 @@ function getPostData() {
 		var posts = makePostGroup(data.posts);
 
 		displayPosts(posts[0]);
-
 		var counter = 0;
 		$("#next").click(function() {
 			
@@ -41,9 +42,29 @@ function getPostData() {
 // Function for animation of different stuff called after the document is loaded 
 
 function animate() {
-	TweenMax.staggerFrom(".element", 1, {scale:0.5, opacity:0, delay:0.2, ease: Bounce.easeOut, force3D:true}, 0.2);
-}
+	var isTweening = true;
+	TweenMax.staggerFrom(".element", 1, {scale:0.5, opacity:0, delay:0.2, onComplete: completed, ease: Bounce.easeOut, force3D:true}, 0.2);
+	function completed () {
+		isTweening = false;
+	}
 
+	$(".element").hover(over, out);
+
+	function over() {
+		if(!isTweening) {
+			isTweening = true;
+			var siblings = $(this).siblings().addClass("a");
+			TweenMax.staggerTo(siblings, 0.3, {scale:0.5, opacity: 0.5, delay:0.3, ease:  Power2.easeIn, force3D:true});
+			//TweenMax.to(this, 0.3, {scale: 1.1, delay: 0.3, ease:  Power2.easeIn});
+		}
+	}
+
+	function out() {
+		var siblings = $(".a");
+		TweenMax.staggerTo(siblings, 0.2, {scale:1, opacity: 1, ease:  Circ.easeIn, force3D:true, overwrite:"all"});
+		isTweening = false;
+	}
+}
 // Function to display the post in each page
 
 function displayPosts( entries ) {
@@ -51,9 +72,11 @@ function displayPosts( entries ) {
 
 	var htmlOutput = template.render(entries);
 
+
 	$("#post-holder").html(htmlOutput);
 	animate();
 }
+
 
 // Function that make the grouping of the posts according to page no;
 
